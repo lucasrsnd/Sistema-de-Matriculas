4aae05aeeb2baa6cf821a75a0d2ab7da3b9b5ccb
Lucas Resende <lucasalves2180@gmail.com>
2025-02-17 15:33:31 -0300

    estrutura das pastas

--- Academico+/Codigo/src/main/java/com.academicoplus/Main.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.academicoplus;
-
-import com.academicoplus.model.Aluno;
-import com.academicoplus.model.Professor;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
